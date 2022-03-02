98442721ba3a57d3df112891ff0d44f3f36f015f
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2022-03-02 22:05:30 +0530

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn, text, command }) => {
 ✯Valor as main team allowed
 ✯No Off topic talks
 ✯No Flirting/Abuses
-✯Dont change grp name/DP
-✯Dont screw up admin
-✯Respect each other and don't argue with anyone else because we are like a family
-✯Any decision should be taken by consult of other fellow admins any one admin can't take any decision themselves
+✯Don\'t change grp name/DP
+✯Don\'t screw up admin
+✯Respect each other and don\'t argue with anyone else because we are like a family
+✯Any decision should be taken by consult of other fellow admins any one admin can\'t take any decision themselves
 ✯Breaking rule=Kickout without warning and admins getting demotion💣
-*Forwarded raids aren't allowed
+*Forwarded raids aren\'t allowed
 *Use mass tag once at the start of the raid and 2nd time after decided', 'https://telegra.ph/file/fe5883c8011fd033e395c.jpg', [
 ['ᴅᴇʟᴇᴛᴇ', '/delete'],
 ['ᴍᴇɴᴜ', '/menu']

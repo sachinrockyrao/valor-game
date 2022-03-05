990514e6dcb46f9cb59f2d15027d6fa286a6d99f
sachinrockyrao <101018641+sachinrockyrao@users.noreply.github.com>
2022-03-06 00:54:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': '493053acc612476f',
+  'https://api.xteam.xyz': 'cc4280950118c6ab',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
   'https://pencarikode.xyz': 'pais',
